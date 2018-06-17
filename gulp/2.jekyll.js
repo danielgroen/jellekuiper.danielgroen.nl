df11b55c769e8455878b287db43a9d23e991f4f6
danielgroen <groendaniel@live.nl>
2018-06-17 13:19:20 +0200

    image compress

--- gulp/2.jekyll.js
+++ gulp/2.jekyll.js
@@ -1,6 +1,9 @@
+'use strict';
+
 const	gulp = require('gulp'),
 		child = require('child_process'),
 		gutil = require('gulp-util');
+
 
 gulp.task('jekyll-serve', () => {
   const jekyll = child.spawn('jekyll', ['serve',
@@ -8,13 +11,11 @@ gulp.task('jekyll-serve', () => {
     '--incremental',
     '--drafts'
   ]);
-
   const jekyllLogger = (buffer) => {
     buffer.toString()
       .split(/\n/)
       .forEach((message) => gutil.log('Jekyll: ' + message));
   };
-
   jekyll.stdout.on('data', jekyllLogger);
   jekyll.stderr.on('data', jekyllLogger);
 });
--- gulp/5.build.js
+++ gulp/5.build.js
@@ -1,19 +1,21 @@
 'use strict';
-
 const	gulp 			= require('gulp'),
 		child 			= require('child_process'),
 		image 			= require("gulp-image"),
 		jpegRecompress 	= require("imagemin-jpeg-recompress"),
 		mozjpeg 		= require("imagemin-mozjpeg"),
 		jpegoptim 		= require('jpegoptim-bin'),
-		gm 				= require('gulp-gm');
+		gm 				= require('gulp-gm'),
+		imageResize = require('gulp-image-resize');
+
+
 
 gulp.task('jekyll-build', () => {
   return child.spawn('jekyll', ['build']);
 });
 
 gulp.task("images:compress", function () {
-    return gulp.src(global.paths.dist + global.paths.imagespath + '**/*')
+    return gulp.src(global.paths.app + global.paths.imagespath + '**/*')
 	    .pipe(image({
 	        jpegRecompress: true,
 	        jpegoptim: false,
@@ -25,11 +27,14 @@ gulp.task("images:compress", function () {
 
 
 gulp.task('images:resize', function () {
-  gulp.src(global.paths.dist + global.paths.imagespath + '**/*')
-    .pipe(gm(function (gmfile) {
-      return gmfile.resize(1920, 1920);
-	  }))
-    .pipe(gulp.dest(global.paths.dist + global.paths.imagespath));
+  return gulp.src(global.paths.app + global.paths.imagespath + '**/*.{png,jpg,jpeg}')
+    .pipe(imageResize({
+      width : 1920,
+      height : 1920,
+      upscale : false,
+      imageMagick: true
+    }))
+    .pipe(gulp.dest(global.paths.app + global.paths.imagespath));
 });
 
-gulp.task('build',  gulp.series('jekyll-build', 'images:resize','images:compress'));
+gulp.task('build',  gulp.series('jekyll-build', 'images:resize', 'images:compress'));
