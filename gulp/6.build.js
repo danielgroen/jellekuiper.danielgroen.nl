c731afb2cc9ef8d5c13869ed12355bae61edad32
danielgroen <groendaniel@live.nl>
2018-09-21 20:59:19 +0200

    focuspoint

--- gulp/6.build.js
+++ gulp/6.build.js
@@ -1,9 +1,32 @@
 'use strict';
 const gulp = require('gulp'),
-  child = require('child_process');
+      child = require('child_process'),
+      replace = require('gulp-replace'),
+      rename = require('gulp-rename'),
+      removeFiles = require('gulp-remove-files'),
+      token = `?v=${(+new Date).toString(36)}`;
+
 
 gulp.task('jekyll-build', () => {
   return child.spawn('jekyll', ['build']);
 });
 
-gulp.task('build', gulp.series('jekyll-build'));
+// Voegt ?v=[randomstring] toe achter kozijnverankering-fallback.js en app.js in index.html tijdens het kopiëren van index.html.
+gulp.task('tokenizeHtml', function (done) {
+
+  // return gulp.src(global.paths.dist + 'index.html')
+  return gulp.src(global.paths.dist + '**/*.html')
+             .pipe(replace('build.js', `build.js${token}`))
+             .pipe(gulp.dest(global.paths.dist));
+});
+
+gulp.task('tokenizeJs', function (done) {
+
+  // return gulp.src(global.paths.dist + 'assets/js/build.js')
+  //            .pipe(rename('build.js' + token))
+  //            .pipe(gulp.dest(global.paths.dist + 'assets/js/'))
+  //            .pipe(removeFiles(global.paths.dist + 'assets/js/build.js'));
+});
+
+
+gulp.task('build', gulp.series('jekyll-build', 'tokenizeHtml'));
