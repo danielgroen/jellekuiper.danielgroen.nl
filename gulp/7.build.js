25d122d4f02f75c39a4d633a096c2633080e93dd
danielgroen <groendaniel@live.nl>
2018-11-02 21:40:34 +0100

    updating npm

--- gulp/7.build.js
+++ gulp/7.build.js
@@ -2,4 +2,4 @@ const gulp = require('gulp'),
       replace = require('gulp-replace'),
       token = `?v=${(+new Date).toString(36)}`;
 
-gulp.task('build', gulp.series('sass','fonts', 'bower', 'js', 'jekyll-build'));
+gulp.task('build', gulp.series('sass','fonts', 'bower', 'js', 'jekyll-build', 'minify'));
