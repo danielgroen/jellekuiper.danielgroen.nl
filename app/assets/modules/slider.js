f21ab11bd90f8b4a1bcc5db523ede75e888fc0cf
danielgroen <groendaniel@live.nl>
2019-03-11 18:14:29 +0100

    fixing owl carousel nav

--- app/assets/modules/slider.js
+++ app/assets/modules/slider.js
@@ -2,15 +2,18 @@
 $(window).ready( function() {
 
 	var owl = $('main .owl-carousel'),
-	    frontOwl = $('.front .owl-carousel');
+      frontOwl = $('.front .owl-carousel'),
+      $pagination = $('.pagination');
 
-	owl.owlCarousel({
+
+  owl.owlCarousel({
 		items: 1,
 		autoplay:true,
     nav: true,
-		autoHeight: true,
+    autoHeight: true,
+    dots: false,
 		navText: ["<svg version='1.1' id='arrow-left' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175' ><path d='M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z'/></svg>","<svg version='1.1' id='arrow-right' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 477.175 477.175'><path d='M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5 c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z'/></svg>"],
-    loop: true,
+    // loop: true,
 		autoplayTimeout:5000,
 		autoplayHoverPause:true
   });
@@ -29,9 +32,14 @@ $(window).ready( function() {
 		autoplayTimeout:10000,
 	});
 
+  $pagination.find('.to').text($('.owl-item').length);
+  
 	owl.on('changed.owl.carousel', function(event) {
-		var owlDots = $('.owl-dots');
-		var activeItem = Math.floor( (owlDots.find('.active').index()) / 10 );
-		owlDots.attr('data-item',activeItem)
+    activeItem = owl.find('.active').index();
+    
+    var currentIndex = event.item.index + 1;
+    $pagination.find('.from').text(currentIndex);
+		// var owlDots = $('.owl-dots');
+		// owlDots.attr('data-item',activeItem)
 	})
 });
